f14f104beacbda4c9379fb37e7a9dc2c51ecc6ff
L11R <savely@krasovsky.me>
2019-08-18 04:05:26 +0300

    Windows compilation fix; deps update

---
+++ rlimit/rlimit.go
@@ -0,0 +1,25 @@
+//+build !windows
+
+package rlimit
+
+import (
+	"github.com/juju/errors"
+	"golang.org/x/sys/unix"
+)
+
+func Set() (err error) {
+	rLimit := unix.RLimit{}
+	err = unix.Getrlimit(unix.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		err = errors.Annotate(err, "Cannot get rlimit")
+		return
+	}
+	rLimit.Cur = rLimit.Max
+
+	err = unix.Setrlimit(unix.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		err = errors.Annotate(err, "Cannot set rlimit")
+	}
+
+	return
+}
