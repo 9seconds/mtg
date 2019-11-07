ed180ecb3dee68b13425bbe49a95e2ee147dfe03
9seconds <nineseconds@yandex.ru>
2019-11-07 14:01:04 +0300

    Remove unnecessary hexify function

--- utils/uint24.go
+++ utils/uint24.go
@@ -1,10 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"strings"
-)
-
 type Uint24 [3]byte
 
 func ToUint24(number uint32) Uint24 {
@@ -13,18 +8,4 @@ func ToUint24(number uint32) Uint24 {
 
 func FromUint24(number Uint24) uint32 {
 	return uint32(number[0]) + (uint32(number[1]) << 8) + (uint32(number[2]) << 16)
-}
-
-func Hexify(data []byte) string {
-	s := []string{}
-
-	for _, v := range data {
-		if v < 0x10 {
-			s = append(s, fmt.Sprintf("0x0%x", v))
-		} else {
-			s = append(s, fmt.Sprintf("0x%x", v))
-		}
-	}
-
-	return strings.Join(s, " ")
 }
