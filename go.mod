b5346668f85cba1f40171256374c322d6a9fcbd5
9seconds <nineseconds@yandex.ru>
2021-03-09 11:57:55 +0300

    Add mock for net conn

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/libp2p/go-reuseport v0.0.2
 	github.com/mccutchen/go-httpbin v1.1.1
 	github.com/pelletier/go-toml v1.8.1
+	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
 )
--- mtglib/network/init_test.go
+++ mtglib/network/init_test.go
@@ -5,11 +5,52 @@ import (
 	"net"
 	"net/http/httptest"
 	"strings"
+	"time"
 
 	"github.com/mccutchen/go-httpbin/httpbin"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
+
+type ConnMock struct {
+	mock.Mock
+}
+
+func (c *ConnMock) Read(b []byte) (int, error) {
+	args := c.Called(b)
+
+	return args.Int(0), args.Error(1)
+}
+
+func (c *ConnMock) Write(b []byte) (int, error) {
+	args := c.Called(b)
+
+	return args.Int(0), args.Error(1)
+}
+
+func (c *ConnMock) Close() error {
+	return c.Called().Error(0)
+}
+
+func (c *ConnMock) LocalAddr() net.Addr {
+	return c.Called().Get(0).(net.Addr)
+}
+
+func (c *ConnMock) RemoteAddr() net.Addr {
+	return c.Called().Get(0).(net.Addr)
+}
+
+func (c *ConnMock) SetDeadline(t time.Time) error {
+	return c.Called(t).Error(0)
+}
+
+func (c *ConnMock) SetReadDeadline(t time.Time) error {
+	return c.Called(t).Error(0)
+}
+
+func (c *ConnMock) SetWriteDeadline(t time.Time) error {
+	return c.Called(t).Error(0)
+}
 
 type DialerMock struct {
 	mock.Mock
