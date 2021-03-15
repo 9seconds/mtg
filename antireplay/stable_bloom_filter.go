cae33a22e65cd023645107af45177577683210d3
9seconds <nineseconds@yandex.ru>
2021-03-15 10:53:29 +0300

    Make stable bloom filter threadsafe

--- antireplay/stable_bloom_filter.go
+++ antireplay/stable_bloom_filter.go
@@ -1,6 +1,8 @@
 package antireplay
 
 import (
+	"sync"
+
 	"github.com/9seconds/mtg/v2/mtglib"
 	"github.com/OneOfOne/xxhash"
 	boom "github.com/tylertreat/BoomFilters"
@@ -8,9 +10,13 @@ import (
 
 type stableBloomFilter struct {
 	filter boom.StableBloomFilter
+	mutex  sync.Mutex
 }
 
 func (s *stableBloomFilter) SeenBefore(digest []byte) bool {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	return s.filter.TestAndAdd(digest)
 }
 
