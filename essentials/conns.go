ef55fbba150f514f693ddd96356daef5f610ff44
9seconds <nineseconds@yandex.ru>
2021-12-01 14:56:39 +0300

    Add documentation for essentials

--- essentials/conns.go
+++ essentials/conns.go
@@ -1,15 +1,26 @@
 package essentials
 
-import "net"
+import (
+	"io"
+	"net"
+)
 
+// CloseableReader is a reader interface that can close its reading end.
 type CloseableReader interface {
+	io.Reader
+
 	CloseRead() error
 }
 
+// CloseableWriter is a writer that can close its writing end.
 type CloseableWriter interface {
+	io.Writer
+
 	CloseWrite() error
 }
 
+// Conn is an extension of net.Conn that can close its ends. This mostly
+// implies TCP connections.
 type Conn interface {
 	net.Conn
 	CloseableReader
