c886ffdd81dd42c74bf62091cc2eb3423f734cac
9seconds <nineseconds@yandex.ru>
2026-03-10 00:25:47 +0100

    Add TrustTLS method to networkHTTPTransport

--- network/v2/http.go
+++ network/v2/http.go
@@ -1,6 +1,9 @@
 package network
 
-import "net/http"
+import (
+	"crypto/tls"
+	"net/http"
+)
 
 type networkHTTPTransport struct {
 	userAgent string
@@ -11,4 +14,12 @@ func (n networkHTTPTransport) RoundTrip(req *http.Request) (*http.Response, erro
 	req.Header.Set("User-Agent", n.userAgent)
 
 	return n.next.RoundTrip(req) //nolint: wrapcheck
+}
+
+func (n networkHTTPTransport) TrustTLS() {
+	tr := n.next.(*http.Transport)
+	if tr.TLSClientConfig == nil {
+		tr.TLSClientConfig = &tls.Config{}
+	}
+	tr.TLSClientConfig.InsecureSkipVerify = true
 }
